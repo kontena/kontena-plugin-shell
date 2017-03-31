e942f4377ab37caa77008d894153d89f4adc4598
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-03-31 17:47:43 +0300

    Hook to allow pasting of stackyaml

---
+++ lib/kontena/plugin/console/callbacks/stack_file.rb
@@ -0,0 +1,31 @@
+require 'tempfile'
+module Kontena
+  module Plugin
+    module Console
+      module Callbacks
+        class StackFile < Kontena::Callback
+
+          matches_commands 'stacks install', 'stacks deploy', 'stacks validate', 'stacks upgrade'
+
+          def before
+            Kontena::Cli::Stacks::YAML::Reader.class_eval do
+              def self.new(*args)
+                if args.first == 'kontena.yml'
+                  @tempfile = Tempfile.new('kontena.yml')
+                  @tempfile.write(Kontena.prompt.multiline("Enter or paste a stack YAML").join)
+                  @tempfile.close
+                  args[0] = @tempfile.path
+                end
+                super *args
+              end
+            end
+          end
+
+          def after
+            @tempfile.unlink if @tempfile
+          end
+        end
+      end
+    end
+  end
+end
