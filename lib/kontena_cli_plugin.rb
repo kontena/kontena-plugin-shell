e6695b198fcf119d779074b66f34d8aa5ec98ad3
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-05-29 10:52:07 +0300

    Speed up startup by delayed loading

--- lib/kontena_cli_plugin.rb
+++ lib/kontena_cli_plugin.rb
@@ -5,6 +5,6 @@ $LOAD_PATH.unshift(path) unless $LOAD_PATH.include?(path)
 require 'kontena_cli' unless Object.const_defined?(:Kontena) && Kontena.const_defined?(:Cli)
 
 require 'kontena/plugin/shell'
-require 'kontena/plugin/shell/shell_command'
+require 'kontena/cli/subcommand_loader'
 
-Kontena::MainCommand.register("shell", "Kontena shell", Kontena::Plugin::Shell::ShellCommand)
+Kontena::MainCommand.register("shell", "Kontena shell", Kontena::Cli::SubcommandLoader.new('kontena/plugin/shell/shell_command'))
