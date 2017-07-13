d7aab61a5fd65ce579700a8ed4d004da88cd863c
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-07-13 15:08:47 +0300

    fix cli versions

--- build/latest.sh
+++ build/latest.sh
@@ -4,8 +4,8 @@ if [ ! -z "$DOCKER_PASSWORD" ] && [ ! -z "$TRAVIS_TAG" ]; then
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
     docker build -t kontena/kosh:latest .
-    docker build --build-arg CLI_VERSION=1.2 -t kontena/kosh:${TRAVIS_TAG}-cli-1.2 .
-    docker build --build-arg CLI_VERSION=1.3 -t kontena/kosh:${TRAVIS_TAG}-cli-1.3 .
+    docker build --build-arg CLI_VERSION=1.2.0 -t kontena/kosh:${TRAVIS_TAG}-cli-1.2 .
+    docker build --build-arg CLI_VERSION=1.3.0 -t kontena/kosh:${TRAVIS_TAG}-cli-1.3 .
 
     docker push kontena/kosh:latest
     docker push kontena/kosh:${TRAVIS_TAG}-cli-1.2
