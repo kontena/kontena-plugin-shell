34a8164c5efba6f8b2b789935236e02767a81e0e
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-07-13 14:21:34 +0300

    fix edge

--- build/edge.sh
+++ build/edge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -z "$DOCKER_PASSWORD" ] && [ ! -z "$TRAVIS_TAG" ]; then
+if [ ! -z "$DOCKER_USERNAME" ] && [ ! -z "$DOCKER_PASSWORD" ]; then
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
     docker build -t kontena/kosh:edge .
     docker push kontena/kosh:edge
