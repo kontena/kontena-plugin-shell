db4e148b221a5129f87cfdcccb68c377ce9c749f
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-10-04 13:12:35 +0300

    Run kontena commands forked to kill any threads left behind or mutated globals

--- spec/kontena/plugin/shell/commands/kontena_spec.rb
+++ spec/kontena/plugin/shell/commands/kontena_spec.rb
@@ -1,7 +1,7 @@
 describe Kontena::Plugin::Shell::KontenaCommand do
   context '#run' do
     it 'should be able to run kontena commands' do
-      expect{described_class.new([], ['version', '--cli']).run}.to output(/cli:/).to_stdout
+      expect{described_class.new([], ['version', '--cli']).run}.to output(/cli:/).to_stdout_from_any_process
     end
 
     it 'should not run a shell inside a shell' do
