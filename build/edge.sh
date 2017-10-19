70cadc04e3cc4d989ccb943c808173ee8a892fc7
Kimmo Lehto <kimmo.lehto@gmail.com>
2017-10-19 10:02:16 +0300

    Fix 1.4 typo

--- build/edge.sh
+++ build/edge.sh
@@ -6,7 +6,7 @@ if [ ! -z "$DOCKER_USERNAME" ] && [ ! -z "$DOCKER_PASSWORD" ]; then
     docker build -t kontena/kosh:edge .
     docker build --build-arg CLI_VERSION=1.2.0 -t kontena/kosh:edge-cli-1.2 .
     docker build --build-arg CLI_VERSION=1.3.0 -t kontena/kosh:edge-cli-1.3 .
-    docker build --build-arg CLI_VERSION=1.4.0 -t kontena/kosh:edge-cli-1 .
+    docker build --build-arg CLI_VERSION=1.4.0 -t kontena/kosh:edge-cli-1.4 .
 
     docker push kontena/kosh:edge
     docker push kontena/kosh:edge-cli-1.2
